efb53bdc2ac2f9258272fa35e0c09c617bfa2e88
VietCT <transonviet2004@gmail.com>
2025-11-27 23:19:11 +0800

    feat(catalog): [backend] Added ProductRepository  (#3)

---
+++ backend/src/main/java/com/vietct/OrderFlow/catalog/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.vietct.OrderFlow.catalog.repository;
+
+import com.vietct.OrderFlow.catalog.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Page<Product> findByCategoryId(UUID categoryId, Pageable pageable);
+}

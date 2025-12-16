c607ca269e6fab3e49493344a0841ab39224e363
VietCT <transonviet2004@gmail.com>
2025-12-16 16:35:52 +0700

    feat(flyway): [backend] Added outbox event.

---
+++ backend/src/main/resources/db/migration/V3__outbox_event.sql
@@ -0,0 +1,14 @@
+CREATE TABLE outbox_event (
+    id UUID PRIMARY KEY,
+    aggregate_type VARCHAR(64) NOT NULL,
+    aggregate_id UUID NOT NULL,
+    event_type VARCHAR(64) NOT NULL,
+    payload JSONB NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    processed_at TIMESTAMPTZ NULL
+);
+
+CREATE INDEX idx_outbox_event_unprocessed
+    ON outbox_event (created_at)
+    WHERE processed_at IS NULL;

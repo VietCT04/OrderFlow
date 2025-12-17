0c921e4b32a436aaf05a4eeb77df963392b98ad2
VietCT <transonviet2004@gmail.com>
2025-12-17 16:19:47 +0700

    feat(catalog): [backend] Added setter category domain.

--- backend/src/main/java/com/vietct/OrderFlow/catalog/domain/Category.java
+++ backend/src/main/java/com/vietct/OrderFlow/catalog/domain/Category.java
@@ -27,7 +27,7 @@ public class Category extends BaseEntity {
     @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     private List<Product> products = new ArrayList<>();
 
-    protected Category() {
+    public Category() {
         // JPA
     }
 
@@ -59,5 +59,21 @@ public class Category extends BaseEntity {
 
     public void changeDescription(String newDescription) {
         this.description = newDescription;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
     }
 }

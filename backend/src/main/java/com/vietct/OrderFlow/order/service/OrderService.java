902908c84995cc0fee0b07184ca9f322b7f5506e
VietCT <transonviet2004@gmail.com>
2025-12-19 14:52:58 +0700

    feat(order): [backend] Added getOrdersForUser method in OrdersService interface.

--- backend/src/main/java/com/vietct/OrderFlow/order/service/OrderService.java
+++ backend/src/main/java/com/vietct/OrderFlow/order/service/OrderService.java
@@ -2,12 +2,14 @@ package com.vietct.OrderFlow.order.service;
 
 import com.vietct.OrderFlow.order.domain.Order;
 import com.vietct.OrderFlow.order.dto.OrderCreateRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface OrderService {
 
     Order placeOrder(OrderCreateRequest request);
-
     Order getOrder(UUID id);
+    Page<Order> getOrdersForUser(UUID userId, Pageable pageable);
 }

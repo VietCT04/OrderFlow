629bf043d620c773e57990db5f3e05f62fed4c5a
VietCT <transonviet2004@gmail.com>
2025-12-20 18:55:00 +0700

    feat(inventory): [backend] Added String form of InsufficientStockException.

--- backend/src/main/java/com/vietct/OrderFlow/inventory/exception/InsufficientStockException.java
+++ backend/src/main/java/com/vietct/OrderFlow/inventory/exception/InsufficientStockException.java
@@ -7,4 +7,7 @@ public class InsufficientStockException extends RuntimeException {
     public InsufficientStockException(UUID productId) {
         super("Insufficient stock for product: " + productId);
     }
+    public InsufficientStockException(String message) {
+        super(message);
+    }
 }
